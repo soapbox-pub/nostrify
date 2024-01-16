5ee367ca06129c9d327c3628d7327db42de66e2f
Alex Gleason <alex@alexgleason.me>
2024-01-16 16:19:28 -0600

    NStore: add opts to every method

--- interfaces/NStore.ts
+++ interfaces/NStore.ts
@@ -2,6 +2,29 @@ import { NostrEvent } from './NostrEvent.ts';
 import { NostrFilter } from './NostrFilter.ts';
 
 export interface NStore<T extends NostrEvent = NostrEvent, F extends NostrFilter = NostrFilter> {
-  add(event: T): Promise<this>;
-  filter(filters: F[]): Promise<T[]>;
+  add(event: T, opts?: NStoreAddOpts): Promise<void>;
+  filter(filters: F[], opts?: NStoreFilterOpts): Promise<T[]>;
+  count?(filters: F[], opts?: NStoreCountOpts): Promise<number>;
+  remove?(filters: F[], opts?: NStoreRemoveOpts): Promise<void>;
+}
+
+export interface NStoreAddOpts {
+  signal?: AbortSignal;
+  relays?: WebSocket['url'][];
+}
+
+export interface NStoreFilterOpts {
+  signal?: AbortSignal;
+  relays?: WebSocket['url'][];
+  limit?: number;
+}
+
+export interface NStoreCountOpts {
+  signal?: AbortSignal;
+  relays?: WebSocket['url'][];
+}
+
+export interface NStoreRemoveOpts {
+  signal?: AbortSignal;
+  relays?: WebSocket['url'][];
 }

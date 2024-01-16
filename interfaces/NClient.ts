cf3adda395e32aeb41c0949a934e243030beded8
Alex Gleason <alex@alexgleason.me>
2024-01-16 16:20:11 -0600

    NRelay -> NClient

--- interfaces/NClient.ts
+++ interfaces/NClient.ts
@@ -2,6 +2,6 @@ import { NostrEvent } from './NostrEvent.ts';
 import { NostrFilter } from './NostrFilter.ts';
 import { NStore } from './NStore.ts';
 
-export interface NRelay extends NStore {
+export interface NClient extends NStore {
   req(filters: NostrFilter[]): AsyncGenerator<NostrEvent>;
 }

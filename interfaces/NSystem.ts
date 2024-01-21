c06d4e1fe3359cbd8e596afaa6329c1daefad121
Alex Gleason <alex@alexgleason.me>
2024-01-21 14:58:45 -0600

    NSystem: add nip05, lnurl caches

--- interfaces/NSystem.ts
+++ interfaces/NSystem.ts
@@ -1,5 +1,25 @@
+import { MapCache } from './MapCache.ts';
 import { NStore } from './NStore.ts';
 
 export interface NSystem {
   events: NStore;
+  nip05: MapCache<string, NProfilePointer, FetchOpts>;
+  lnurl: MapCache<string, LNDetails, FetchOpts>;
+}
+
+interface NProfilePointer {
+  pubkey: string;
+  relays?: WebSocket['url'][];
+}
+
+interface LNDetails {
+  callback: string;
+  maxSendable: number;
+  minSendable: number;
+  metadata: string;
+  tag: 'payRequest';
+}
+
+interface FetchOpts {
+  signal?: AbortSignal | null;
 }

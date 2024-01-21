fa83368f73318fe24677e2f61419ce0a574edfb7
Alex Gleason <alex@alexgleason.me>
2024-01-21 13:52:32 -0600

    NStore: filter -> query

--- interfaces/NStore.ts
+++ interfaces/NStore.ts
@@ -3,7 +3,7 @@ import { NostrFilter } from './NostrFilter.ts';
 
 export interface NStore<T extends NostrEvent = NostrEvent, F extends NostrFilter = NostrFilter> {
   save(event: T, opts?: NStoreAddOpts): Promise<void>;
-  filter(filters: F[], opts?: NStoreFilterOpts): Promise<T[]>;
+  query(filters: F[], opts?: NStoreFilterOpts): Promise<T[]>;
   count?(filters: F[], opts?: NStoreCountOpts): Promise<number>;
   remove?(filters: F[], opts?: NStoreRemoveOpts): Promise<void>;
 }

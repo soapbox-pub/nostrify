fe209cf0d1829b783b0db9bd42c79917a3af3b88
Alex Gleason <alex@alexgleason.me>
2024-01-21 15:24:20 -0600

    NClient: add generics

--- interfaces/NClient.ts
+++ interfaces/NClient.ts
@@ -2,6 +2,6 @@ import { NostrEvent } from './NostrEvent.ts';
 import { NostrFilter } from './NostrFilter.ts';
 import { NStore } from './NStore.ts';
 
-export interface NClient extends NStore {
-  req(filters: NostrFilter[]): AsyncGenerator<NostrEvent>;
+export interface NClient<T extends NostrEvent, F extends NostrFilter> extends NStore<T, F> {
+  req(filters: F[]): AsyncGenerator<T>;
 }

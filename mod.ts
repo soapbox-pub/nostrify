057ecc6e2ce813db6e2279288fbfd08c5b53cc0c
Alex Gleason <alex@alexgleason.me>
2024-01-23 10:37:49 -0600

    mod.ts: export NStoreOpts

--- mod.ts
+++ mod.ts
@@ -6,4 +6,4 @@ export type { MapCache } from './interfaces/MapCache.ts';
 export type { NostrEvent } from './interfaces/NostrEvent.ts';
 export type { NostrFilter } from './interfaces/NostrFilter.ts';
 export type { NProfilePointer } from './interfaces/NProfilePointer.ts';
-export type { NStore } from './interfaces/NStore.ts';
+export type { NStore, NStoreOpts } from './interfaces/NStore.ts';

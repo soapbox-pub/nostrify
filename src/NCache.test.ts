8eb932d9feac295d10dd2f67fa55b13180a73ed2
Alex Gleason <alex@alexgleason.me>
2024-01-23 17:21:04 -0600

    Add NCache test

---
+++ src/NCache.test.ts
@@ -0,0 +1,22 @@
+import { assertEquals } from 'https://deno.land/std@0.212.0/assert/assert_equals.ts';
+
+import { NCache } from './NCache.ts';
+
+import event1 from '../fixtures/event.json' assert { type: 'json' };
+
+Deno.test('NCache', async () => {
+  const cache = new NCache({
+    max: 3000,
+    maxEntrySize: 5000,
+    sizeCalculation: (event) => JSON.stringify(event).length,
+  });
+
+  assertEquals(await cache.count([{ ids: [event1.id] }]), 0);
+
+  await cache.event(event1);
+
+  assertEquals(await cache.count([{ ids: [event1.id] }]), 1);
+
+  const result = await cache.query([{ ids: [event1.id] }]);
+  assertEquals(result[0], event1);
+});

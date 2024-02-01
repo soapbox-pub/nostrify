8bcc5da5b31303a5dda9ab10cb4842ba6fabfa8c
Alex Gleason <alex@alexgleason.me>
2024-02-01 14:44:27 -0600

    Remove NSystem and MapCache

--- interfaces/MapCache.ts
+++
@@ -1,4 +0,0 @@
-export interface MapCache<K, V, O> {
-  fetch(key: K, opts?: O): Promise<V>;
-  put(key: K, value: V): Promise<void>;
-}
--- mod.ts
+++ mod.ts
@@ -6,7 +6,6 @@ export { NSecSigner } from './src/NSecSigner.ts';
 export { NSet } from './src/NSet.ts';
 
 export type { LNURLDetails } from './interfaces/LNURLDetails.ts';
-export type { MapCache } from './interfaces/MapCache.ts';
 export type { NostrEvent } from './interfaces/NostrEvent.ts';
 export type { NostrFilter } from './interfaces/NostrFilter.ts';
 export type { NostrSigner } from './interfaces/NostrSigner.ts';

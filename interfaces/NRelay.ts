b62c874af33b99f5b84378406bfc8b05e987bd6d
Alex Gleason <alex@alexgleason.me>
2024-02-01 17:04:54 -0600

    NRelay: simplify type

--- interfaces/NRelay.ts
+++ interfaces/NRelay.ts
@@ -2,7 +2,6 @@ import { NostrFilter } from './NostrFilter.ts';
 import { NostrRelayCLOSED, NostrRelayEOSE, NostrRelayEVENT } from './NostrRelayMsg.ts';
 import { NStore, NStoreOpts } from './NStore.ts';
 
-export interface NRelay extends WebSocket, NStore {
-  cmd(cmd: [verb: string, ...unknown[]]): Promise<void>;
+export interface NRelay extends NStore {
   req(filters: NostrFilter[], opts?: NStoreOpts): AsyncGenerator<NostrRelayEVENT | NostrRelayEOSE | NostrRelayCLOSED>;
 }

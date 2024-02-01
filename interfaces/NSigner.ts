857a8340f5b2b906f1141adb9eaf3be3d7522551
Alex Gleason <alex@alexgleason.me>
2024-01-31 20:55:54 -0600

    Add NSigner interface

---
+++ interfaces/NSigner.ts
@@ -0,0 +1,17 @@
+import { NostrEvent } from './NostrEvent.ts';
+
+export interface NSigner {
+  /** Returns a public key as hex. */
+  getPublicKey(): Promise<string>;
+  /** Takes an event template, adds `id`, `pubkey` and `sig` and returns it. */
+  signEvent(event: Omit<NostrEvent, 'id' | 'pubkey' | 'sig'>): Promise<NostrEvent>;
+  /** Returns a record of relay URLs to relay policies. */
+  getRelays?(): Promise<Record<WebSocket['url'], { read: boolean; write: boolean }>>;
+  /** @deprecated NIP-04 crypto methods. */
+  nip04?: {
+    /** @deprecated Returns ciphertext and iv as specified in NIP-04. */
+    encrypt(pubkey: string, plaintext: string): Promise<string>;
+    /** @deprecated Takes ciphertext and iv as specified in NIP-04. */
+    decrypt(pubkey: string, ciphertext: string): Promise<string>;
+  };
+}
--- mod.ts
+++ mod.ts
@@ -9,4 +9,5 @@ export type { MapCache } from './interfaces/MapCache.ts';
 export type { NostrEvent } from './interfaces/NostrEvent.ts';
 export type { NostrFilter } from './interfaces/NostrFilter.ts';
 export type { NProfilePointer } from './interfaces/NProfilePointer.ts';
+export type { NSigner } from './interfaces/NSigner.ts';
 export type { NStore, NStoreOpts } from './interfaces/NStore.ts';

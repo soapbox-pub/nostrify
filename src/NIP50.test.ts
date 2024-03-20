e077172f2276d0f554879206e27196953dc8ca25
Alex Gleason <alex@alexgleason.me>
2024-03-19 22:03:57 -0500

    Add NIP-50 search module

---
+++ src/NIP50.test.ts
@@ -0,0 +1,31 @@
+import { assertEquals } from 'https://deno.land/std@0.212.0/assert/mod.ts';
+
+import { NIP50 } from './NIP50.ts';
+
+Deno.test('NIP50.parseInput', () => {
+  assertEquals(NIP50.parseInput(''), []);
+  assertEquals(NIP50.parseInput(' '), []);
+  assertEquals(NIP50.parseInput('hello'), ['hello']);
+  assertEquals(NIP50.parseInput('hello world'), ['hello', 'world']);
+  assertEquals(NIP50.parseInput('hello "world"'), ['hello', 'world']);
+  assertEquals(NIP50.parseInput('hello "world" "hello world"'), [
+    'hello',
+    'world',
+    'hello world',
+  ]);
+  assertEquals(NIP50.parseInput('domain:gleasonator.dev'), [
+    { key: 'domain', value: 'gleasonator.dev' },
+  ]);
+  assertEquals(NIP50.parseInput('domain:gleasonator.dev '), [
+    { key: 'domain', value: 'gleasonator.dev' },
+  ]);
+  assertEquals(
+    NIP50.parseInput('name:John "New York" age:30 hobbies:programming'),
+    [
+      { key: 'name', value: 'John' },
+      'New York',
+      { key: 'age', value: '30' },
+      { key: 'hobbies', value: 'programming' },
+    ],
+  );
+});
---
+++ src/NIP50.ts
@@ -0,0 +1,24 @@
+type SearchToken = string | { key: string; value: string };
+
+/** [NIP-50](https://github.com/nostr-protocol/nips/blob/master/50.md) search functionality. */
+export class NIP50 {
+  static parseInput(input: string): SearchToken[] {
+    const regex = /(\b\w+:[^\s"]+)|(".*?")|(\S+)/g;
+
+    const tokens: SearchToken[] = [];
+    let match: RegExpExecArray | null;
+
+    while ((match = regex.exec(input)) !== null) {
+      if (match[1]) {
+        const [key, value] = match[1].split(':');
+        tokens.push({ key, value });
+      } else if (match[2]) {
+        tokens.push(match[2].replace(/"/g, ''));
+      } else if (match[3]) {
+        tokens.push(match[3]);
+      }
+    }
+
+    return tokens;
+  }
+}

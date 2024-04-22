1a25e7622679185fa78bcc0cdb4591fe0bacef83
Alex Gleason <alex@alexgleason.me>
2024-04-22 15:03:43 -0500

    Add PowPolicy

--- src/policies/mod.ts
+++ src/policies/mod.ts
@@ -4,4 +4,5 @@ export { HellthreadPolicy } from './HellthreadPolicy.ts';
 export { KeywordPolicy } from './KeywordPolicy.ts';
 export { NoOpPolicy } from './NoOpPolicy.ts';
 export { PipelinePolicy } from './PipelinePolicy.ts';
+export { PowPolicy } from './PowPolicy.ts';
 export { ReadOnlyPolicy } from './ReadOnlyPolicy.ts';

57bbdf14b697ca80d232f5de0d1b8552cba8f5d6
Alex Gleason <alex@alexgleason.me>
2024-04-24 15:38:36 -0500

    NConnectSigner: implement most methods

--- src/NConnectSigner.ts
+++ src/NConnectSigner.ts
@@ -1,24 +1,21 @@
 // deno-lint-ignore-file require-await
 
+import { NostrConnectRequest, NostrConnectResponse } from '../interfaces/NostrConnect.ts';
 import { NostrEvent } from '../interfaces/NostrEvent.ts';
 import { NostrSigner } from '../interfaces/NostrSigner.ts';
 import { NRelay } from '../interfaces/NRelay.ts';
 
-import { NSecSigner } from './NSecSigner.ts';
-
-interface NConnectMsg {
-  id: string;
-  method: string;
-  params: string | string[];
-}
+import { NSchema as n } from './NSchema.ts';
 
 interface NConnectSignerOpts {
   /** Relay to facilitate connection. */
   relay: NRelay;
   /** Remote pubkey to sign as. */
   pubkey: string;
-  /** Local key to connect with. */
-  secretKey: Uint8Array;
+  /** Local signer to sign the request events. */
+  signer: NostrSigner;
+  /** Timeout for requests. */
+  timeout?: number;
 }
 
 /** [NIP-46](https://github.com/nostr-protocol/nips/blob/master/46.md) remote signer through a relay. */
@@ -26,24 +23,46 @@ export class NConnectSigner implements NostrSigner {
   private relay: NRelay;
   private pubkey: string;
   private signer: NostrSigner;
-  private connected: boolean = false;
+  private timeout?: number;
 
-  constructor({ relay, pubkey, secretKey }: NConnectSignerOpts) {
+  private connected: boolean = false;
+  private ee = new EventTarget();
+
+  constructor({ relay, pubkey, signer, timeout }: NConnectSignerOpts) {
     this.relay = relay;
     this.pubkey = pubkey;
-    this.signer = new NSecSigner(secretKey);
+    this.signer = signer;
+    this.timeout = timeout;
   }
 
   private async connect(): Promise<void> {
-    
   }
 
-  private async send(msg: NConnectMsg, signal: AbortSignal): Promise<NConnectMsg> {
+  /** High-level RPC method. Returns the string result, or throws on error. */
+  private async cmd(method: string, params: string[]): Promise<string> {
+    const signal = typeof this.timeout === 'number' ? AbortSignal.timeout(this.timeout) : undefined;
+
+    const { result, error } = await this.send(
+      { id: method, method, params },
+      { signal },
+    );
+
+    if (error) {
+      throw new Error(error);
+    }
+
+    return result;
+  }
+
+  /** Low-level send method. Deals directly with connect request/response. */
+  private async send(request: NostrConnectRequest, opts: { signal?: AbortSignal } = {}): Promise<NostrConnectResponse> {
+    const { signal } = opts;
+
     const event = await this.signer.signEvent({
       kind: 24133,
-      content: JSON.stringify(msg),
+      content: JSON.stringify(request),
       created_at: Math.floor(Date.now() / 1000),
-      tags: [],
+      tags: [['p', this.pubkey]],
     });
 
     const req = this.relay.req(
@@ -51,40 +70,48 @@ export class NConnectSigner implements NostrSigner {
       { signal },
     );
 
-    this.relay.event(event);
+    this.relay.event(event, { signal });
 
     for await (const msg of req) {
-      if (msg[0] === 'EVENT') return JSON.parse(msg[2].content);
+      if (msg[0] === 'EVENT') {
+        const event = msg[2];
+        const decrypted = await this.signer.nip04!.decrypt(this.pubkey, event.content);
+        const response = n.json().pipe(n.connectResponse()).parse(decrypted);
+        if (response.id === request.id) {
+          return response;
+        }
+      }
     }
 
     throw new Error('No response from relay');
   }
 
   async getPublicKey(): Promise<string> {
-    throw new Error('Not implemented');
+    return this.cmd('get_public_key', []);
   }
 
   async signEvent(event: Omit<NostrEvent, 'id' | 'pubkey' | 'sig'>): Promise<NostrEvent> {
-    throw new Error('Not implemented');
+    const result = await this.cmd('sign_event', [JSON.stringify(event)]);
+    return n.json().pipe(n.event()).parse(result);
   }
 
   readonly nip04 = {
     encrypt: async (pubkey: string, plaintext: string): Promise<string> => {
-      throw new Error('Not implemented');
+      return this.cmd('nip04_encrypt', [pubkey, plaintext]);
     },
 
     decrypt: async (pubkey: string, ciphertext: string): Promise<string> => {
-      throw new Error('Not implemented');
+      return this.cmd('nip04_decrypt', [pubkey, ciphertext]);
     },
   };
 
   readonly nip44 = {
     encrypt: async (pubkey: string, plaintext: string): Promise<string> => {
-      throw new Error('Not implemented');
+      return this.cmd('nip44_encrypt', [pubkey, plaintext]);
     },
 
     decrypt: async (pubkey: string, ciphertext: string): Promise<string> => {
-      throw new Error('Not implemented');
+      return this.cmd('nip44_decrypt', [pubkey, ciphertext]);
     },
   };
 }

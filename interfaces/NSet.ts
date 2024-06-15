54fcba101cb0064f98bd93c37a427b39374b6002
Alex Gleason <alex@alexgleason.me>
2024-06-15 12:56:21 -0500

    deno lint

--- interfaces/NSet.ts
+++ interfaces/NSet.ts
@@ -1,5 +1,4 @@
 import { NostrEvent } from './NostrEvent.ts';
 
 /** Nostr event implementation of the `Set` interface. */
-// deno-lint-ignore no-empty-interface
 export interface NSet extends Set<NostrEvent> {}

a4af545fe87b27177aea8844c89d48b4615d45b5
Alex Gleason <alex@alexgleason.me>
2024-09-08 10:17:30 -0500

    Add ReplyBotPolicy to block replies from bots in the same second

--- packages/nostrify/test/mod.ts
+++ packages/nostrify/test/mod.ts
@@ -1,1 +1,17 @@
+import { NostrEvent } from '@nostrify/types';
+import { finalizeEvent, generateSecretKey } from 'nostr-tools';
+
 export { MockRelay } from './MockRelay.ts';
+
+/** Generate an event for use in tests. */
+export function genEvent(t: Partial<NostrEvent> = {}, sk: Uint8Array = generateSecretKey()): NostrEvent {
+  const { id, kind, pubkey, tags, content, created_at, sig } = finalizeEvent({
+    kind: 255,
+    created_at: 0,
+    content: '',
+    tags: [],
+    ...t,
+  }, sk);
+
+  return { id, kind, pubkey, tags, content, created_at, sig };
+}

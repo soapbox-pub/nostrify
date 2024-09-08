8391f9b4f0fedc01e5171bcdb0d57adaeb05b33f
Alex Gleason <alex@alexgleason.me>
2024-09-08 10:27:20 -0500

    Move genEvent and jsonlEvents to @nostrify/nostrify/test

--- packages/nostrify/test/mod.ts
+++ packages/nostrify/test/mod.ts
@@ -3,6 +3,12 @@ import { finalizeEvent, generateSecretKey } from 'nostr-tools';
 
 export { MockRelay } from './MockRelay.ts';
 
+/** Import a JSONL fixture by name in tests. */
+export async function jsonlEvents(path: string): Promise<NostrEvent[]> {
+  const data = await Deno.readTextFile(path);
+  return data.split('\n').map((line) => JSON.parse(line));
+}
+
 /** Generate an event for use in tests. */
 export function genEvent(t: Partial<NostrEvent> = {}, sk: Uint8Array = generateSecretKey()): NostrEvent {
   const { id, kind, pubkey, tags, content, created_at, sig } = finalizeEvent({

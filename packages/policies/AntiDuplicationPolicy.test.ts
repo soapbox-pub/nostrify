8f6af2d4427f1371bcfddb3f7f94271907676eca
Alex Gleason <alex@alexgleason.me>
2024-09-23 13:02:15 -0500

    AntiDuplicationPolicy: add deobfuscate option

--- packages/policies/AntiDuplicationPolicy.test.ts
+++ packages/policies/AntiDuplicationPolicy.test.ts
@@ -1,31 +1,50 @@
+import { genEvent } from '@nostrify/nostrify/test';
 import { assertEquals } from '@std/assert';
-import { finalizeEvent, generateSecretKey } from 'nostr-tools';
 
 import { AntiDuplicationPolicy } from './AntiDuplicationPolicy.ts';
 
 Deno.test('AntiDuplicationPolicy', async () => {
-  const kv = await Deno.openKv(':memory:');
+  using kv = await Deno.openKv(':memory:');
 
   const policy = new AntiDuplicationPolicy({ kv });
   const content = 'Spicy peppermint apricot mediterranean ginger carrot spiced juice edamame hummus';
 
-  const event = finalizeEvent(
-    { kind: 1, content, tags: [], created_at: 0 },
-    generateSecretKey(),
-  );
+  const event1 = genEvent({ kind: 1, content });
 
-  assertEquals((await policy.call(event))[2], true);
-  assertEquals((await policy.call(event))[2], false);
-  assertEquals((await policy.call(event))[2], false);
+  assertEquals((await policy.call(event1))[2], true);
+  assertEquals((await policy.call(event1))[2], false);
+  assertEquals((await policy.call(event1))[2], false);
 
-  const event2 = finalizeEvent(
-    { kind: 1, content: 'a', tags: [], created_at: 0 },
-    generateSecretKey(),
-  );
+  const event2 = genEvent({ kind: 1, content: 'a' });
 
   assertEquals((await policy.call(event2))[2], true);
   assertEquals((await policy.call(event2))[2], true);
   assertEquals((await policy.call(event2))[2], true);
+});
 
-  kv.close();
+Deno.test('AntiDuplicationPolicy with deobfuscate', async () => {
+  using kv = await Deno.openKv(':memory:');
+
+  const policy = new AntiDuplicationPolicy({
+    kv,
+    deobfuscate: ({ content }) => (
+      content
+        .replace(/[\p{Emoji}\p{Emoji_Modifier}\p{Emoji_Component}\p{Emoji_Modifier_Base}\p{Emoji_Presentation}]/gu, '')
+        .replace(/\s/g, '')
+        .toLowerCase()
+    ),
+  });
+
+  const event1 = genEvent({
+    kind: 1,
+    content: 'Spicy peppermint apricot mediterranean ginger carrot spiced juice edamame hummus',
+  });
+
+  const event2 = genEvent({
+    kind: 1,
+    content: 'Spicy 🌶️  peppermint apricot 🍑 mediterranean 🌊 ginger carrot 🥕 spiced 🌶️ juice 🥤 edamame 🌱  hummus ',
+  });
+
+  assertEquals((await policy.call(event1))[2], true);
+  assertEquals((await policy.call(event2))[2], false);
 });

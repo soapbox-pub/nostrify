7b25370ebec11de133daa61cb56596b6eec6b407
Alex Gleason <alex@alexgleason.me>
2025-02-05 22:02:18 -0600

    NIP-50: parse negated tokens

--- packages/nostrify/NIP50.test.ts
+++ packages/nostrify/NIP50.test.ts
@@ -8,24 +8,27 @@ Deno.test('NIP50.parseInput', () => {
   assertEquals(NIP50.parseInput('hello'), ['hello']);
   assertEquals(NIP50.parseInput('hello world'), ['hello', 'world']);
   assertEquals(NIP50.parseInput('hello "world"'), ['hello', 'world']);
-  assertEquals(NIP50.parseInput('hello "world" "hello world"'), [
-    'hello',
-    'world',
-    'hello world',
-  ]);
-  assertEquals(NIP50.parseInput('domain:gleasonator.dev'), [
-    { key: 'domain', value: 'gleasonator.dev' },
-  ]);
-  assertEquals(NIP50.parseInput('domain:gleasonator.dev '), [
-    { key: 'domain', value: 'gleasonator.dev' },
-  ]);
-  assertEquals(NIP50.parseInput('domain: yolo'), [
-    'domain:',
-    'yolo',
-  ]);
-  assertEquals(NIP50.parseInput('domain:localhost:8000'), [
-    { key: 'domain', value: 'localhost:8000' },
-  ]);
+
+  assertEquals(
+    NIP50.parseInput('hello "world" "hello world"'),
+    ['hello', 'world', 'hello world'],
+  );
+
+  assertEquals(
+    NIP50.parseInput('domain:gleasonator.dev'),
+    [{ key: 'domain', value: 'gleasonator.dev' }],
+  );
+
+  assertEquals(
+    NIP50.parseInput('domain: yolo'),
+    ['domain:', 'yolo'],
+  );
+
+  assertEquals(
+    NIP50.parseInput('domain:localhost:8000'),
+    [{ key: 'domain', value: 'localhost:8000' }],
+  );
+
   assertEquals(
     NIP50.parseInput('name:John "New York" age:30 hobbies:programming'),
     [
@@ -34,5 +37,22 @@ Deno.test('NIP50.parseInput', () => {
       { key: 'age', value: '30' },
       { key: 'hobbies', value: 'programming' },
     ],
+  );
+});
+
+Deno.test('NIP50.parseInput with negated token', () => {
+  assertEquals(
+    NIP50.parseInput('-reply:true'),
+    [{ key: '-reply', value: 'true' }],
+  );
+
+  assertEquals(
+    NIP50.parseInput('hello -reply:true'),
+    ['hello', { key: '-reply', value: 'true' }],
+  );
+
+  assertEquals(
+    NIP50.parseInput('-media:true -reply:true'),
+    [{ key: '-media', value: 'true' }, { key: '-reply', value: 'true' }],
   );
 });
--- packages/nostrify/NIP50.ts
+++ packages/nostrify/NIP50.ts
@@ -3,19 +3,19 @@ type SearchToken = string | { key: string; value: string };
 /** [NIP-50](https://github.com/nostr-protocol/nips/blob/master/50.md) search functionality. */
 export class NIP50 {
   static parseInput(input: string): SearchToken[] {
-    const regex = /(\b\w+:[^\s"]+)|(".*?")|(\S+)/g;
+    const regex = /(\B-\w+:[^\s"]+)|(\b\w+:[^\s"]+)|(".*?")|(\S+)/g;
 
     const tokens: SearchToken[] = [];
     let match: RegExpExecArray | null;
 
     while ((match = regex.exec(input)) !== null) {
-      if (match[1]) {
-        const [key, ...values] = match[1].split(':');
+      if (match[1] || match[2]) {
+        const [key, ...values] = (match[1] || match[2]).split(':');
         tokens.push({ key, value: values.join(':') });
-      } else if (match[2]) {
-        tokens.push(match[2].replace(/"/g, ''));
       } else if (match[3]) {
-        tokens.push(match[3]);
+        tokens.push(match[3].replace(/"/g, ''));
+      } else if (match[4]) {
+        tokens.push(match[4]);
       }
     }
 

edd1e0f4a400c8147602a3f97c0ad2483998f023
Alex Gleason <alex@alexgleason.me>
2025-03-18 22:12:42 -0500

    Copy in nostr-context.tsx from Soapbox

---
+++ packages/react/nostr-context.tsx
@@ -0,0 +1,56 @@
+import { NRelay1 } from '@nostrify/nostrify';
+import { createContext, useContext, useEffect, useState } from 'react';
+
+import { useInstance } from 'soapbox/hooks/useInstance.ts';
+
+interface NostrContextType {
+  relay?: NRelay1;
+  isRelayLoading: boolean;
+}
+
+const NostrContext = createContext<NostrContextType | undefined>(undefined);
+
+interface NostrProviderProps {
+  children: React.ReactNode;
+}
+
+export const NostrProvider: React.FC<NostrProviderProps> = ({ children }) => {
+  const { instance } = useInstance();
+
+  const [relay, setRelay] = useState<NRelay1>();
+  const [isRelayLoading, setIsRelayLoading] = useState(true);
+
+  const relayUrl = instance.nostr?.relay;
+
+  const handleRelayOpen = () => {
+    setIsRelayLoading(false);
+  };
+
+  useEffect(() => {
+    if (relayUrl) {
+      const relay = new NRelay1(relayUrl);
+      relay.socket.underlyingWebsocket.addEventListener('open', handleRelayOpen);
+      setRelay(relay);
+    } else {
+      setIsRelayLoading(false);
+    }
+    return () => {
+      relay?.socket.underlyingWebsocket.removeEventListener('open', handleRelayOpen);
+      relay?.close();
+    };
+  }, [relayUrl]);
+
+  return (
+    <NostrContext.Provider value={{ relay, isRelayLoading }}>
+      {children}
+    </NostrContext.Provider>
+  );
+};
+
+export const useNostr = () => {
+  const context = useContext(NostrContext);
+  if (context === undefined) {
+    throw new Error('useNostr must be used within a NostrProvider');
+  }
+  return context;
+};

2bd4aa34762d73c2e39d7637ae89c92258c721d3
Alex Gleason <alex@alexgleason.me>
2025-03-23 16:05:33 -0500

    Add feed on example site

--- packages/react/example/App.tsx
+++ packages/react/example/App.tsx
@@ -3,36 +3,50 @@ import { useNostr, useNostrEvents } from '@nostrify/react';
 function App() {
   const { user, login } = useNostr();
 
-  const { events, error } = useNostrEvents(
+  const { events: [author] } = useNostrEvents(
     user ? [{ kinds: [0], authors: [user.pubkey] }] : [],
   );
 
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  }
+  const { events: notes } = useNostrEvents([{ kinds: [1], limit: 20 }]);
 
-  if (user) {
-    const [author] = events;
+  function renderLogin() {
+    if (user) {
+      if (author) {
+        const { name } = JSON.parse(author.content);
+        return <div>Welcome back, {name}!</div>;
+      }
 
-    if (author) {
-      const { name } = JSON.parse(author.content);
-      return <div>Welcome back, {name}!</div>;
+      return <div>You: {user.pubkey}</div>;
     }
 
-    return <div>You: {user.pubkey}</div>;
-  }
+    if (login.isLoading) {
+      return <div>Loading...</div>;
+    }
 
-  if (login.isLoading) {
-    return <div>Loading...</div>;
+    return (
+      <button
+        type='button'
+        onClick={() => login.extension()}
+      >
+        Log in
+      </button>
+    );
   }
 
   return (
-    <button
-      type='button'
-      onClick={() => login.extension()}
-    >
-      Log in
-    </button>
+    <div>
+      <h2>Login</h2>
+      {renderLogin()}
+
+      <h2>Notes</h2>
+      {notes.map((note) => (
+        <div key={note.id}>
+          {note.content}
+          <br />
+          <br />
+        </div>
+      ))}
+    </div>
   );
 }
 

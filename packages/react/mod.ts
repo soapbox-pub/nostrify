c0433c5f89ed16968a2ae964d3a00719318381dd
Alex Gleason <alex@alexgleason.me>
2025-03-27 13:55:52 -0500

    Remove useNostrEvents and IdbRelay for now

--- packages/react/mod.ts
+++ packages/react/mod.ts
@@ -1,4 +1,3 @@
 export { NostrContext, type NostrContextType } from './NostrContext.ts';
 export { NostrProvider } from './NostrProvider.tsx';
 export { useNostr } from './useNostr.ts';
-export { useNostrEvents } from './useNostrEvents.ts';

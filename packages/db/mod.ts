41c6948cb561468b3d90c1fdbaa677c8074ed8dc
Siddharth Singh <me@shantaram.xyz>
2025-07-29 17:20:04 +0530

    migrate to pnpm workspaces

--- packages/db/mod.ts
+++ packages/db/mod.ts
@@ -1,2 +1,5 @@
-export { NDatabase, type NDatabaseOpts, type NDatabaseSchema } from './NDatabase.ts';
-export { NPostgres, type NPostgresOpts, type NPostgresSchema } from './NPostgres.ts';
+export {
+    NPostgres,
+    type NPostgresOpts,
+    type NPostgresSchema,
+} from "./NPostgres.ts";
